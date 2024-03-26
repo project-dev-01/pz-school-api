e0a54f8569e5a999970cbeed8f5d63de42b7dfde
karthiksure31 <karthiksure31@gmail.com>
2024-03-26 19:48:53 +0530

    added navbar

--- config/constants.php
+++ config/constants.php
@@ -1,16 +1,11 @@
 <?php
 return [
-    'main_db'=>'alive_jskl_livedb',
-    'client_email'=>'askyourquery@paxsuzen.com',
+    'main_db'=>'apaxsuzenapi',
+    'client_email'=>'askyourquery@suzen.school',
     'student_email_domain' => "@jskl.edu.my",
-    
     'cache_time'=>"24",
-
     'cache_academic_years'=>"academic_years_",
     'cache_departments'=>"departments_",
     'cache_classes'=>"classes_",    
     'cache_sections'=>"sections_",
-    
-    
-    
 ];
