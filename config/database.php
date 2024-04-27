5e95f023809f8d34e11cad6eee468d4b294249cb
Surendhar <surendhar@aibots.my>
2024-04-27 21:46:30 +0530

    Bug Working on A-203

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'live_api'),
+            'database' => env('DB_DATABASE', 'pz-school'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
